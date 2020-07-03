1c245f9e1b2ece1175f47b28ab8cb63be58f6b6f
Nicholas Robinson <nicholasrobinson@NicholasMacBook.localdomain>
2020-07-03 12:28:23 -0700

    Adding power control via mosfet to prevent binding

--- README.md
+++ README.md
@@ -37,9 +37,10 @@ I wanted to control a servo to latch and unlatch my dog door using Homebridge. T
 
 # Example dog_door_latch.py
 
-You will need to specify the GPIO pin and the pulse width for the off/on positions:
+You will need to specify the GPIO pins for power/pwm and the pulse width for the off/on positions:
 ```
-GPIO_PIN = 12
+GPIO_POWER_PIN = 4
+GPIO_PWM_PIN = 12
 UNLOCK_PULSE_WIDTH = 500
 LOCK_PULSE_WIDTH = 1400
 ```
--- dog_door_latch.py
+++ dog_door_latch.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
 import sys
+import time
 import pigpio
 
 # Note: Pinout
 # https://i.stack.imgur.com/yHddo.png
 # Note: Hardware PWM available on GPIO12, GPIO13, GPIO18, GPIO19 
 # https://www.raspberrypi.org/documentation/usage/gpio/
-GPIO_PIN = 12
+GPIO_POWER_PIN = 4
+GPIO_PWM_PIN = 12
 UNLOCK_PULSE_WIDTH = 500
 LOCK_PULSE_WIDTH = 1400
 
+GPIO_HIGH = 1
+GPIO_LOW = 0
+
 pi = pigpio.pi()
 
-pi.set_mode(GPIO_PIN, pigpio.OUTPUT)
+pi.set_mode(GPIO_POWER_PIN, pigpio.OUTPUT)
+pi.set_mode(GPIO_PWM_PIN, pigpio.OUTPUT)
 
+pi.write(GPIO_POWER_PIN, GPIO_HIGH)
 if sys.argv[-1] == 'LATCH':
     # Latch
-    pi.set_servo_pulsewidth(GPIO_PIN, LOCK_PULSE_WIDTH)
+    pi.set_servo_pulsewidth(GPIO_PWM_PIN, LOCK_PULSE_WIDTH)
 else:
     # Unlatch
-    pi.set_servo_pulsewidth(GPIO_PIN, UNLOCK_PULSE_WIDTH)
+    pi.set_servo_pulsewidth(GPIO_PWM_PIN, UNLOCK_PULSE_WIDTH)
+time.sleep(1)
+pi.write(GPIO_POWER_PIN, GPIO_LOW)
 
 pi.stop()
